af96d2e335432be90c0f7433cdbce258f48c62a5
Miguel Oliveira <55458349+Miguel-Oliveiraa@users.noreply.github.com>
2022-07-02 19:32:53 -0300

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
-
+//teste
 const TelaDois = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
